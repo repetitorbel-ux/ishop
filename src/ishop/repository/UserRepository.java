63dca2a5af5e977c8110124e012f994bd5aae3f3
Viktor Irchits <repetitorbel@gmail.com>
2025-12-08 11:42:42 +0300

    User authentication has been created, and the code has been partially refactored

--- src/ishop/repository/UserRepository.java
+++ src/ishop/repository/UserRepository.java
@@ -2,11 +2,14 @@ package ishop.repository;
 
 import ishop.entity.User;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
 
+    /** Создать класс(ы) с исключением для вывода теста ошибки???? */
 
+    //Метод, выполняющий сериализацию
     public static void serialize(List<User> userList, String nameOfFile){
 //        String nameOfFile = "UserList";
 
@@ -17,21 +20,19 @@ public class UserRepository {
         }
     }
 
-    //Создать класс с исключением для вывода теста ошибки, если файл отсутствует????
 
+    //Метод, выполняющий десериализацию
     public static List<User> deserialize(String nameOfFile){
 
         try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(nameOfFile))) {
             List<User> userList = (List<User>) objectInputStream.readObject();
             return userList;
-        }catch (IOException e) {// Если файл отсутствует или недоступен
+        } catch (FileNotFoundException e){
+            return new ArrayList<>();
+        } catch (IOException e) {// Если файл отсутствует или недоступен
             throw new RuntimeException(e);//Выкидываем исходное исключение в RuntimeException, чтобы не скрывать информацию об ошибке
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 }
-/*
-1.Сначала сделать в репозитории метод по добавлению пользователя
-2.Потом в сервисе создать этот репозитории и вызвать на нем метод по добавлению пользователя
- */
