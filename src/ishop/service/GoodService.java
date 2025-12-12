6321c3649408b056246dcb17b04cd80b7e6c32d7
Viktor Irchits <repetitorbel@gmail.com>
2025-12-12 14:16:16 +0300

    Update goods; small refactoring

--- src/ishop/service/GoodService.java
+++ src/ishop/service/GoodService.java
@@ -21,8 +21,21 @@ public class GoodService {
         return goodExistList;
     }
 
-    //Ввод товара и создание коллекции
-    public static Good entryGood(String path, int id){
+    //Метод, сериализующий товар
+    public static void writeGood(List<Good> goodList, String path) {
+        GoodRepository goodRepository = new GoodRepository();
+        goodRepository.serializeGood(goodList, path);
+    }
+
+    //Метод поиска максимального значения в списке
+    private static Optional<Good> findMaxGoodId (List < Good > goodList) {
+        return goodList.stream().max(Comparator.comparing(good -> {
+            return good.getId();
+        }));
+    }
+
+    //Ввод товара и создание коллекции  (4 - Добавить товар)
+    public static Good entryGood(String path, int id) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -41,8 +54,8 @@ public class GoodService {
         System.out.println("Введите цену товара");
         int priceGood = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Задайте ограничение по возрасту товара (true/false)");
-        boolean ageLimit = Boolean.parseBoolean(scanner.nextLine());
+        System.out.println("Задайте ограничение по возрасту товара");
+        int ageLimit = Integer.parseInt(scanner.nextLine());
 
         //Создание нового товара и коллекции товаров
         Good goodNew = new Good(id, nameGood, codeGood, brandGood, categoryGood, priceGood, ageLimit);
@@ -53,19 +66,14 @@ public class GoodService {
         writeGood(goodList, path);//вызов метода, сериализующего товар
         return goodNew; //нигде не используется
     }
-    /** Изменить тип entryGood() на void????  */
+    /**      * Изменить тип entryGood() на void????    */
 
-    //Метод, сериализующий товар
-    public static void writeGood(List<Good> goodList, String path){
-        GoodRepository goodRepository = new GoodRepository();
-        goodRepository.serializeGood(goodList, path);
-    }
-
-    public static void addGood(List<Good> goodExistList, String path){
+    //Метод, добавляющий товар (4 - Добавить товар)
+    public static void addGood(List<Good> goodExistList, String path) {
         if (goodExistList.isEmpty()) {
 //            entryGood(goodExistList, path, 1);
             entryGood(path, 1);
-        }else {
+        } else {
             Optional<Good> idGoodMax = findMaxGoodId(goodExistList);
             if (idGoodMax.isPresent()) {
                 Good idMax = idGoodMax.get();
@@ -73,7 +81,6 @@ public class GoodService {
                 int idNext = idGoodMax.get().getId() + 1;
                 System.out.println("Следующий id = " + idNext);
 
-//                Good goodNext = entryGood(goodExistList, path, idNext);
                 Good goodNext = entryGood(path, idNext);
 
                 //Создание коллекции товаров
@@ -94,8 +101,8 @@ public class GoodService {
         }
     }
 
-    //Метод, выводящий все товары
-    public static void showGoods(List<Good> goodList, String path){
+    //Метод, выводящий все товары (1 - Показать список всех товаров)
+    public static void showGoods(List<Good> goodList, String path) {
         goodList = getGoodListFromFile(path);
         System.out.println("Список товаров:");
         for (Good good : goodList) {
@@ -103,8 +110,8 @@ public class GoodService {
         }
     }
 
-    //Метод, проверяющий введенной с консоли категории товара
-    public static void checkCategory(List<Good> goodList, String path, String targetCategory){
+    //Метод, проверяющий введенной с консоли категории товара (2 - Показать товары по категориям)
+    public static void checkCategory(List<Good> goodList, String path, String targetCategory) {
         goodList = getGoodListFromFile(path);
         System.out.println();
         String categoryNotExist = null;
@@ -113,12 +120,12 @@ public class GoodService {
                 categoryNotExist = targetCategory;
             }
         }
-        if(categoryNotExist == null){
+        if (categoryNotExist == null) {
             System.out.println("Категории товара " + '\'' + targetCategory + '\'' + " не существует. Введите корректное наименование категории");
-        }else showGoodsByCategory(goodList, targetCategory);
+        } else showGoodsByCategory(goodList, targetCategory);
     }
 
-    //Метод, выводящий товары определенной категории
+    //Метод, выводящий товары определенной категории (2 - Показать товары по категориям)
     public static void showGoodsByCategory(List<Good> goodList, String targetCategory) {
         System.out.println("Список товаров в категории " + targetCategory + ": ");
         for (Good good : goodList) {
@@ -128,21 +135,138 @@ public class GoodService {
         }
     }
 
-    //Метод, выводящий товары сортированные по категории и затем по цене
-    public static void sortGoodByCategoryAndPrice(List<Good> goodList, String path){
+    //Метод, выводящий товары сортированные по категории и затем по цене (3 - Показать товары по стоимости и категориям (с сортировкой))
+    public static void sortGoodByCategoryAndPrice (List < Good > goodList, String path){
         System.out.println("Before sort: " + goodList);
+        System.out.println();
         goodList = getGoodListFromFile(path);
         goodList.sort(Comparator.comparing((Good good1) -> good1.getCategory())
                 .thenComparing(good -> good.getPrice()));
         System.out.println("After sort: " + goodList);
     }
 
-    //Метод поиска максимального значения в списке
-    private static Optional<Good> findMaxGoodId(List<Good> goodList) {
-        return goodList.stream().max(Comparator.comparing(good -> {
-            return good.getId();
-        }));
+
+    public static void updateGoodById(List<Good> goodList, String path) {
+        goodList = getGoodListFromFile(path);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nВведите id товара: ");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        Good good = findById(goodList, id);
+        if (good == null) {
+            System.out.println("Товар не найден.");
+            return;
+        }
+
+        System.out.println("Выбранный товар: " + good);
+
+        changeGood(good);  //Вызываем метод, который изменяет объект внутри списка
+
+        writeGood(goodList, path); //Сохраняем изменения в файл
+
+        System.out.println("\nИзменения сохранены.");
     }
+
+    //Метод поиска товара по id
+    public static Good findById(List<Good> list, int id) {
+        for (Good g : list) {
+            if (g.getId() == id) return g;
+        }
+        return null;
+    }
+
+    public static void changeGood(Good good) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nВыберете действие: \n" + "1 - Изменить название товара \n"
+                    + "2 - Изменить код товара \n"
+                    + "3 - Изменить бренд товара \n"
+                    + "4 - Изменить категорию товара \n"
+                    + "5 - Изменить цену товара \n"
+                    + "6 - Изменить возрастное ограничение товара \n"
+                    + "0. Выход \n");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("Введите новое название:");
+                    good.setName(scanner.nextLine());
+                    break;
+                case "2":
+                    System.out.println("Введите новый код:");
+                    good.setCode(scanner.nextLine());
+                    break;
+                case "3":
+                    System.out.println("Введите новый бренд:");
+                    good.setBrand(scanner.nextLine());
+                    break;
+                case "4":
+                    System.out.println("Введите новую категорию:");
+                    good.setCategory(scanner.nextLine());
+                    break;
+                case "5":
+                    System.out.println("Введите новую цену:");
+                    good.setPrice(Integer.parseInt(scanner.nextLine()));
+                    break;
+                case "6":
+                    System.out.println("Введите новое возрастное ограничение:");
+                    good.setAgeLimit(Integer.parseInt(scanner.nextLine()));
+                    break;
+                case "0":
+                    System.out.println("Выход из меню обновления товара.");
+                    return;
+                default:
+                    System.out.println("Неверный ввод. Повторите.");
+            }
+        }
+    }
+//****************************to Delete***********************************************
+    //Пробный - For delete
+    public static void changeGoodOld(Good good, int id){
+        System.out.println("Наименование товара до изменения: " + '\'' + good.getName() + '\'');
+        good.setName("Велосипед");
+        System.out.println("Наименование товара после изменения: " + '\'' + good.getName() + '\'');
+    }
+
+    //Метод, выводящий товары определенного индентификатора - для удаления
+    public static void showGoodsById(List<Good> goodList, String path, int id) {
+        goodList = getGoodListFromFile(path);
+        System.out.println("Товар c id " + id + ": ");
+        for (Good good : goodList) {
+            if (good.getId() == id) {
+                System.out.println(good);
+//                scaner(good, path);
+            }
+        }
+    }
+
+    //Метод scaner: для удаления
+    public static void scaner(Good good, String path) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("\nВыберете действие: \n" + "1 - Изменение названия товара \n"
+                + "2 - Изменение категории товара \n"
+                + "3 - Изменение цены товара \n");
+        String entry = scanner.nextLine();
+        switch (entry) {
+            case "1":
+//                changeGood(good, path);
+                break;
+            case "2":
+//                entryCategory(goodList, path);
+                break;
+            default:
+                System.out.println("Неверный ввод");
+
+        }
+    }
+
+
+
+
 
 }
 //        Optional<Good> idGoodMax = findMaxGoodId(goodExistList);
@@ -199,3 +323,16 @@ public class GoodService {
 //            entryGood(goodExistList, path, 1);
 //       }
 //
+
+/*
+public static Good showGoods(List<Good> goodList, String path) {
+        goodList = getGoodListFromFile(path);
+        System.out.println("Список товаров:");
+        Good goods = null;
+        for (Good good : goodList) {
+            System.out.println(good);
+            goods = good;
+        }
+        return goods;
+    }
+ */
